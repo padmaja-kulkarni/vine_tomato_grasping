9f2f3f19023eb2aef78c17dfa49dc13c865f32ed
taeke <taeke.dehaan@live.nl>
2020-05-21 11:52:52 +0200

    Added a package for pointing at the tomaotes, needs a lot of cleaning!

---
+++ flex_point/src/object_detection.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 15:30:31 2020
+
+@author: jelle
+"""
+
+import sys
+sys.path.append('/home/taeke/catkin_ws/src/flexcraft_jelle/flex_grasp/src/func/')
+
+import rospy
+
+from cv_bridge import CvBridge, CvBridgeError
+
+# msg
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import PoseStamped
+
+from flex_grasp.msg import Tomato
+from flex_grasp.msg import Truss
+from flex_grasp.msg import Peduncle
+
+# custom func
+from detect_crop.ProcessImage import ProcessImage
+from utils import camera_info2intrinsics
+
+import pyrealsense2 as rs
+
+# import pathlib
+import os # os.sep
+
+class ObjectDetection(object):
+    """ObjectDetection"""
+    def __init__(self):
+
+        self.event = None
+
+        self.color_image = None
+        self.depth_image = None
+        self.depth_info = None
+        self.color_info = None
+        self.trans = None
+        self.init = None
+
+        self.bridge = CvBridge()
+        self.camera_frame = "camera_color_optical_frame"
+
+        pathCurrent = os.path.dirname(__file__) # path to THIS file
+        self.pwdProcess = os.path.join(pathCurrent, '..', '..', 'results')
+
+        rospy.loginfo("Storing visiual results in: ", self.pwdProcess)
+
+        self.debug_mode = rospy.get_param("object_detection/debug")
+
+        if self.debug_mode:
+            log_level = rospy.DEBUG
+            rospy.loginfo("[OBJECT DETECTION] Luanching object detection node in debug mode")
+        else:
+            log_level = rospy.INFO
+
+        rospy.init_node("object_detection", anonymous=True, log_level=log_level)
+
+        # Publish
+        self.pub_e_out = rospy.Publisher("~e_out",
+                                         String, queue_size=10, latch=True)
+
+        self.pub_object_features = rospy.Publisher("object_features",
+                                        Truss, queue_size=5, latch=True)
+                                        
+        self.pub_segment_image = rospy.Publisher("segment_image",
+                                        Image, queue_size=5, latch=True)
+
+        # Subscribe
+        rospy.Subscriber("~e_in", String, self.e_in_cb)
+
+        if not self.debug_mode:
+            rospy.Subscriber("camera/color/image_raw", Image, self.color_image_cb)
+            rospy.Subscriber("camera/depth/image_rect_raw", Image, self.depth_image_cb)
+            rospy.Subscriber("camera/color/camera_info", CameraInfo, self.color_info_cb)
+            rospy.Subscriber("camera/color/camera_info", CameraInfo, self.depth_info_cb)
+
+    def e_in_cb(self, msg):
+        if self.event is None:
+            self.event = msg.data
+            rospy.logdebug("[OBJECT DETECTION] Received object detection event message: %s", self.event)
+
+            msg = String()
+            msg.data = ""
+            self.pub_e_out.publish(msg)
+
+    def color_image_cb(self, msg):
+        if (self.color_image is None) and (self.event == "e_start"):
+            rospy.logdebug("[OBJECT DETECTION] Received color image message")
+            try:
+                self.color_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
+            except CvBridgeError as e:
+                print(e)
+
+    def depth_image_cb(self, msg):
+        if (self.depth_image is None) and (self.event == "e_start"):
+            rospy.logdebug("[OBJECT DETECTION] Received depth image message")
+            try:
+                self.depth_image = self.bridge.imgmsg_to_cv2(msg, "passthrough")  /1000.0
+            except CvBridgeError as e:
+                print(e)
+
+    def color_info_cb(self, msg):
+        if (self.color_info is None) and (self.event == "e_start"):
+            rospy.logdebug("[OBJECT DETECTION] Received color info message")
+            self.color_info = msg
+
+    def depth_info_cb(self, msg):
+        if (self.depth_info is None) and (self.event == "e_start"):
+            rospy.logdebug("[OBJECT DETECTION] Received depth info message")
+            self.depth_info = msg
+
+    def received_all_data(self):
+        return (self.color_image is not None) and (self.depth_image is not None) and (self.depth_info is not None) and (self.color_info is not None)
+
+    def clear_all_data(self):
+        self.color_image = None
+        self.depth_image = None
+        self.depth_info = None
+        self.color_info = None
+
+    def wait_for_data(self, time_out):
+        while not self.received_all_data():
+            rospy.sleep(0.1)
+        return True
+
+    def detect_object(self):
+
+
+        if self.wait_for_data(5):
+            pwd = os.path.dirname(__file__)
+
+
+            image = ProcessImage(self.color_image, tomatoName = 'gazebo_tomato',
+                                 pwdProcess = pwd,
+                                 saveIntermediate = False)
+
+            # Image processing
+            image.segment_img_2()
+            image.detect_tomatoes_global()
+            
+            # get results
+            segment_image = image.get_segmented_image()
+            tomato_features = image.get_tomatoes()
+            
+            # get camera properties
+            intrin = camera_info2intrinsics(self.depth_info)
+
+            #%%#############
+            ### tomatoes ###
+            ################
+            tomatoes = []
+
+            # rospy.logdebug("cols: %s [px]", col)
+            for i in range(0, len(tomato_features['col'])):
+
+                # Load from struct
+                col = tomato_features['col'][i]
+                row = tomato_features['row'][i]
+                radius = tomato_features['radii'][i]
+
+                point = self.deproject(row, col, intrin)
+
+                depth = self.depth_image[(row, col)]
+                point1 = rs.rs2_deproject_pixel_to_point(intrin, [0,0], depth)
+                point2 = rs.rs2_deproject_pixel_to_point(intrin, [0,radius], depth)
+                radius_m = euclidean(point1, point2)
+
+                tomatoes.append(point_to_tomato(point, radius_m, self.camera_frame))
+
+            #%%#############
+            ### Peduncle ###
+            ################
+            cage_pose = PoseStamped()
+            peduncle = Peduncle()
+
+            truss = self.create_truss(tomatoes, cage_pose, peduncle)
+            
+            # publish results
+            imgmsg = self.bridge.cv2_to_imgmsg(segment_image, encoding="rgb8")
+            self.pub_segment_image.publish(imgmsg)
+            self.pub_object_features.publish(truss)
+
+            return True
+        else:
+            rospy.logwarn("Did not receive all data")
+            return False
+
+    def create_truss(self, tomatoes, cage_pose, peduncle):
+        #%%##########
+        ### Truss ###
+        #############
+        truss = Truss()
+        truss.tomatoes = tomatoes
+        truss.cage_location = cage_pose
+        truss.peduncle = peduncle
+
+        return truss
+
+    def deproject(self, row, col, intrin):
+        # Deproject
+        index = (row, col)
+        depth = self.depth_image[index]
+        # rospy.logdebug("Corresponding depth: %s", self.depth_image[index])
+        # https://github.com/IntelRealSense/librealsense/wiki/Projection-in-RealSense-SDK-2.0
+
+        pixel = [float(col), float(row)]
+        depth = float(depth)
+
+        point = rs.rs2_deproject_pixel_to_point(intrin, pixel, depth)
+        return point
+
+    def take_action(self):
+        success = None
+        msg = String()
+
+        if (self.event == "e_start"):
+            success = self.detect_object()
+
+        elif (self.event == "e_init"):
+            self.init = True
+            success = True
+
+        # publish success
+        if success is not None:
+            if success == True:
+                self.clear_all_data()
+                msg.data = "e_success"
+                self.event = None
+
+            elif success == False:
+                msg.data = "e_failure"
+                rospy.logwarn("Object detection failed to execute %s", self.event)
+                self.event = None
+
+            self.pub_e_out.publish(msg)
+
+def euclidean(v1, v2):
+    return sum((p-q)**2 for p, q in zip(v1, v2)) ** .5
+
+
+def point_to_tomato(point, radius, frame):
+
+    tomato = Tomato()
+    tomato.header.frame_id = frame
+    tomato.header.stamp = rospy.Time.now()
+
+    tomato.position.x = point[0]
+    tomato.position.y = point[1]
+    tomato.position.z = point[2] + radius
+
+    tomato.radius = radius
+    return tomato
+
+
+
+def main():
+    try:
+        object_detection = ObjectDetection()
+        rospy.loginfo("[OBJECT DETECTION] Initialized")
+        rate = rospy.Rate(10)
+
+        while not rospy.core.is_shutdown():
+            object_detection.take_action()
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+if __name__ == '__main__':
+    main()
---
+++ flex_point/src/pose_transform.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 15:49:29 2020
+
+@author: jelle
+"""
+
+import sys
+sys.path.insert(0, '/home/taeke/catkin_ws/src/flexcraft_jelle/flex_grasp/src/')
+
+import numpy as np
+
+import rospy
+
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+from flex_grasp.msg import Truss
+from std_msgs.msg       import Float64
+
+from func.conversions import point_to_pose_stamped
+from func.conversions import pose_to_lists
+from func.utils import add_lists, multiply_lists
+from moveit_commander.conversions import list_to_pose
+from math import pi
+from func.conversions import list_to_orientation
+
+from tf.transformations import quaternion_from_euler
+from func.ros_utils import wait_for_success
+
+import tf2_ros
+import tf2_geometry_msgs
+
+class PoseTransform(object):
+    """PoseTransform"""
+    def __init__(self):
+        self.event = None
+        self.object_features = None
+
+        self.debug_mode = rospy.get_param("pose_transform/debug")
+
+        if self.debug_mode:
+            log_level = rospy.DEBUG
+            rospy.loginfo("[POSE TRANSFORM] Luanching object detection node in debug mode")
+        else:
+            log_level = rospy.INFO
+
+        rospy.init_node("pose_transform",
+                        anonymous=True, log_level=log_level)
+
+        # Initialize Subscribers
+        rospy.Subscriber("object_features", Truss, self.object_features_cb)
+        rospy.Subscriber("~e_in", String, self.e_in_cb)
+
+        # Initialize Publishers
+        self.pub_move_robot_command = rospy.Publisher("/px150/move_robot/e_in",
+                          String, queue_size=10, latch=False)
+
+        self.pub_pre_grasp_pose = rospy.Publisher('pre_grasp_pose',
+                                PoseStamped, queue_size=5, latch=True)
+
+        latch = True
+        self.pub_e_out = rospy.Publisher("~e_out",
+                                         String, queue_size=10, latch=latch)
+
+        self.use_iiwa = rospy.get_param('use_iiwa')
+        self.use_interbotix = rospy.get_param('use_interbotix')
+        self.use_sdh = rospy.get_param('use_sdh')
+
+        if self.use_iiwa:
+            self.grasp_position_transform = [0, 0, -0.06] # [m]
+            self.pre_grasp_position_transform = [0, 0, 0.05] # [m]
+            self.orientation_transform = [0, 0, -pi/2]
+        if self.use_interbotix:
+            self.grasp_position_transform =     [0, 0, 0.04] # [m]
+            self.pre_grasp_position_transform = [0, 0, 0.08] # [m]
+            self.orientation_transform = [-pi, pi/2, 0]
+
+
+        self.place_orientation_transform = [1.0, 1.0, -1.0]
+        self.place_position_transform = [0.0, 0.0, 0.0]
+
+        if self.use_interbotix:
+
+            self.pre_grasp_ee = {
+                "left_finger"   :0.03,
+                "right_finger"  :-0.03}
+
+            self.grasp_ee = {
+                "left_finger"   :0.015,
+                "right_finger"  :-0.015}
+        if self.use_iiwa:
+
+            self.pre_grasp_ee = {
+                "sdh_finger_12_joint"   :-0.7,
+                "sdh_finger_13_joint"   :1.57,
+                "sdh_finger_21_joint"   :0,
+                "sdh_finger_22_joint"   :-0.7,
+                "sdh_finger_23_joint"   :1.57,
+                "sdh_knuckle_joint"     :0,
+                "sdh_thumb_2_joint"     :-0.7,
+                "sdh_thumb_3_joint"     : 1.57}
+
+            self.grasp_ee = {
+                "sdh_finger_12_joint"   :0,
+                "sdh_finger_13_joint"   :1.57,
+                "sdh_finger_21_joint"   :0,
+                "sdh_finger_22_joint"   :0,
+                "sdh_finger_23_joint"   :1.57,
+                "sdh_knuckle_joint"     :0,
+                "sdh_thumb_2_joint"     :0,
+                "sdh_thumb_3_joint"     :1.57}
+
+        # Listen
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
+
+        rospy.set_param('pre_grasp_ee', self.pre_grasp_ee)
+        rospy.set_param('grasp_ee', self.grasp_ee)
+
+        #wait until transform is obtained
+        self.trans = None
+
+
+    def object_features_cb(self, msg):
+        if self.object_features is None:
+            self.object_features = msg
+            rospy.logdebug("[POSE TRANSFORM] Received new object feature message")
+
+    def e_in_cb(self, msg):
+        if self.event is None:
+            self.event = msg.data
+            rospy.logdebug("[POSE TRANSFORM] Received new pose transform event message %s", self.event)
+
+            msg = String()
+            msg.data = ""
+            self.pub_e_out.publish(msg)
+            # self.pub_e_out.publish(msg_e)
+            # msg_e = String()
+            # msg_e.data = "e_wait"
+
+    def get_trans(self):
+        if not (self.object_features is None):
+            try:
+                frame = self.object_features.tomatoes[0].header.frame_id
+                self.trans = self.tfBuffer.lookup_transform('world',frame,  rospy.Time(0))
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                pass
+            # continue
+
+    def transform_pose(self):
+        if self.object_features is None:
+            rospy.logwarn("[POSE TRANSFORM] Cannot transform pose, since object_features still empty!")
+            return False
+        else:
+
+            while self.trans is None:
+                rospy.logdebug("[POSE TRANSFORM] wating for transform")
+                self.get_trans()
+                rospy.sleep(0.5)
+                
+            for tomato in self.object_features.tomatoes:
+                
+                tomato_pose = PoseStamped()
+                tomato_pose.header = tomato.header
+                tomato_pose.pose.position = tomato.position
+                tomato_pose.pose.orientation = quaternion_from_euler(0,0,0)
+
+                tomato_pose = point_to_pose_stamped([tomato.position.x, tomato.position.y, tomato.position.z], [0,0,0], tomato.header.frame_id, tomato.header.stamp)
+
+                self.object_pose = tf2_geometry_msgs.do_transform_pose(tomato_pose, self.trans)
+                self.object_pose.pose.position.z = 0.1  
+                ai = 0.0
+                aj = 0.0
+                ak = np.arctan(self.object_pose.pose.position.y/self.object_pose.pose.position.x)
+                self.object_pose.pose.orientation = list_to_orientation([ai, aj, ak])
+                
+                pre_grasp_pose = self.object_pose_to_grasp_pose(self.pre_grasp_position_transform)
+
+
+                self.pub_pre_grasp_pose.publish(pre_grasp_pose)
+                self.pub_move_robot_command.publish("move")
+    
+                # get response
+                success = wait_for_success("/px150/move_robot/e_out", 5)
+
+                self.pub_move_robot_command.publish("home")
+                success = wait_for_success("/px150/move_robot/e_out", 5)
+
+            # reset
+            self.object_features = None
+            return success
+
+    def object_pose_to_grasp_pose(self, position_transform):
+
+        object_pose = self.object_pose
+        grasp_pose = PoseStamped()
+        grasp_pose.header = object_pose.header
+
+        # position
+        object_position, object_orientation = pose_to_lists(object_pose.pose, 'euler')
+        grasp_position = add_lists(object_position, position_transform)
+        grasp_orientation = add_lists(object_orientation, self.orientation_transform)
+
+        grasp_pose.pose = list_to_pose(grasp_position + grasp_orientation)
+
+        return grasp_pose
+
+    def object_pose_to_place_pose(self, grasp_pose):
+
+        place_pose = PoseStamped()
+        place_pose.header = grasp_pose.header
+
+        # position
+        grasp_position, grasp_orientation = pose_to_lists(grasp_pose.pose, 'euler')
+        place_position = add_lists(grasp_position, self.place_position_transform)
+        place_orientation = multiply_lists(grasp_orientation, self.place_orientation_transform)
+
+        place_pose.pose = list_to_pose(place_position + place_orientation)
+
+        return place_pose
+        # return self.object_pose_to_grasp_pose(height)
+
+    def take_action(self):
+
+        success = None
+        msg = String()
+
+        if self.event == "e_start":
+            success = self.transform_pose()
+
+        elif self.event == "e_init":
+            rospy.logdebug("[POSE TRANSFORM] executing e_init command")
+            success = True
+
+        # publish success
+        if success is not None:
+            if success == True:
+                msg.data = "e_success"
+                self.event = None
+
+            elif success == False:
+                msg.data = "e_failure"
+                rospy.logwarn("Pose transform failed to execute: %s", self.event)
+                self.event = None
+
+            self.pub_e_out.publish(msg)
+
+def main():
+    try:
+        pose_transform = PoseTransform()
+        rate = rospy.Rate(10)
+
+        while not rospy.core.is_shutdown():
+            pose_transform.take_action()
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+if __name__ == '__main__':
+    main()
