4383a3a3b2ddb2e6589688c6bf9d06e3a65747d8
taeke <taeke.dehaan@live.nl>
2020-07-24 11:36:03 +0200

    some more chnages for detect_crop -> detect_truss, tested and works.

--- detect_truss/setup.py
+++ detect_truss/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['detect_crop'],
+    packages=['detect_truss'],
     package_dir={'': 'src'}
 )
 setup(**d)
