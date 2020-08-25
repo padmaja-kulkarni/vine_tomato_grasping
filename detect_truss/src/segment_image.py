e9a3dfaf46a3df3c362f2243d7f9eb1f471d54a1
taeke <taeke.dehaan@live.nl>
2020-08-25 16:26:11 +0200

    Added pid control

--- detect_truss/src/segment_image.py
+++ detect_truss/src/segment_image.py
@@ -19,7 +19,7 @@ from detect_truss.segment_image import segment_truss
 # ls | cat -n | while read n f; do mv "$f" `printf "%03d.png" $n`; done
 if __name__ == '__main__':
 
-    N = 5           # tomato file to load
+    N = 6           # tomato file to load
     extension = ".png"
     dataset = "failures" # "tomato_rot" #  "depth_blue" #
     save = True
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     
     count = 0
     
-    for i_tomato in range(1, N):
+    for i_tomato in range(5, N):
     
         tomato_ID = str(i_tomato).zfill(3)
         tomato_name = tomato_ID
