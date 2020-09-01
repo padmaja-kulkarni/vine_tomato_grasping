4517f1430dabdcb0f2998051952118de1acfd248
taeke <taeke.dehaan@live.nl>
2020-09-01 17:08:11 +0200

    Removed rqt_image_processing

--- rqt_image_processing/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['rqt_image_processing'], # rqt_sdh_grasp
-    package_dir={'': 'src'},
-)
-
-setup(**d)
