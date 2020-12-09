c1364ad49acb45ed2352f4dd0d102fafa0e3e859
taeke de haan <taeke.dehaan@live.nl>
2020-12-09 12:25:45 +0100

    Polish experiment path interface.

--- rqt_user_interface/src/rqt_user_interface/experiment_path_interface.py
+++ rqt_user_interface/src/rqt_user_interface/experiment_path_interface.py
@@ -5,12 +5,11 @@ from std_msgs.msg import String
 
 
 class ExperimentPathInterface(object):
-    experiment_name_options = ['default', 'simple', 'moderate', 'advanced']
+    EXPERIMENT_NAME_OPTIONS = ['default', 'simple', 'moderate', 'advanced']
+    LOG_FOLDER = 'thesis_data'
+    NEW_STRING = "  (new)"
 
     def __init__(self, experiment_name_button, experiment_id_button):
-
-        self.experiment_name_path = None
-        self.experiment_id_path = None
 
         self.experiment_name_button = experiment_name_button
         self.experiment_id_button = experiment_id_button
@@ -18,73 +17,88 @@ class ExperimentPathInterface(object):
         self.experiment_name = None
         self.experiment_id = None
         self.pub_experiment_pwd = rospy.Publisher("experiment_pwd", String, queue_size=1, latch=True)
+        self.prev_experiment_ids = []
 
         # experiment name dropdown
-
-        initialize_drop_down_button(self.experiment_name_button, self.experiment_name_options, self.handle_experiment_name)
+        initialize_drop_down_button(self.experiment_name_button, self.EXPERIMENT_NAME_OPTIONS, self.handle_experiment_name)
         self.handle_experiment_name()
 
         # experiment id dropdown
         initialize_drop_down_button(self.experiment_id_button,  self.get_experiment_ids(), self.handle_experiment_id)
         self.handle_experiment_id()
 
-        self.experiment_id_path = os.path.join(self.experiment_name_path, self.experiment_id)
-        self.pub_experiment_pwd.publish(self.experiment_id_path)
+        self.publish_experiment_path()
 
-    def handle_experiment_name(self):
+
+    def handle_experiment_name(self, button):
         """
-            callback called when experiment name is changed, updates experiment id and publishes path
+            callback called when experiment name is changed, updates experiment name
         """
         value = str(self.experiment_name_button.currentText())
         if self.experiment_name != value:
             self.experiment_name = value
             rospy.logdebug("Updated experiment name to %s", self.experiment_name)
 
-        self.update_experiment_name_path()
-
+        # Update id button even if experiment name dit not change,
         self.update_experiment_id_button()
         self.handle_experiment_id()
 
     def handle_experiment_id(self):
-        print "experiment_id_button"
-
+        """
+            callback called when experiment name or id is changed, updates experiment id and publishes path
+        """
+        self.update_experiment_id_button()
         value = str(self.experiment_id_button.currentText())[0:3]
 
         if self.experiment_id != value:
             self.experiment_id = value
             rospy.logdebug("Updated experiment id to %s", self.experiment_id)
+            self.publish_experiment_path()
 
-            self.update_experiment_name_path()
-            self.experiment_id_path = os.path.join(self.experiment_name_path, self.experiment_id)
-            self.pub_experiment_pwd.publish(self.experiment_id_path)
+    def update_experiment_id_button(self):
+        """
+            updates the options in the experiment_id_button if they changed compared to previous call
+        """
+        # get ids
+        experiment_ids = self.get_experiment_ids()
+
+        # add to menu when changed
+        if self.prev_experiment_ids != experiment_ids:
+
+            self.experiment_id_button.clear()
+            for index, experiment_id in enumerate(experiment_ids):
+                self.experiment_id_button.addItem(experiment_id)
+
+                if experiment_id == self.experiment_id:
+                    self.experiment_id_button.setCurrentIndex(100)
+
+            self.prev_experiment_ids = experiment_ids
 
     def get_experiment_ids(self):
+        """
+            returns a list of ids available in the experiment_name_path, and an additional id which creates a new folder
+        """
+        experiment_name_path = self.get_experiment_name_path()
+
         options = []
-        if not os.path.isdir(self.experiment_name_path):
-            options.append(str(1).zfill(3) + "  (new)")
+        if not os.path.isdir(experiment_name_path):
+            options.append(str(1).zfill(3) + self.NEW_STRING)
         else:
-            contents = os.listdir(self.experiment_name_path)
+            contents = os.listdir(experiment_name_path)
             if len(contents) == 0:
-                options.append(str(1).zfill(3) + " (new)")
+                options.append(str(1).zfill(3) + self.NEW_STRING)
             else:
                 contents.sort()
                 for file_name in contents:
                     file_id = int(file_name[:3])
                     options.append(str(file_id).zfill(3))
-                options.append(str(file_id + 1).zfill(3) + " (new)")
+                options.append(str(file_id + 1).zfill(3) + self.NEW_STRING)
 
         return options
 
-    def update_experiment_id_button(self):
-        button = self.experiment_id_button
+    def get_experiment_name_path(self):
+        return os.path.join(os.getcwd(), self.LOG_FOLDER, self.experiment_name)
 
-        # get ids
-        options = self.get_experiment_ids()
-
-        # add to menu
-        button.clear()
-        for option in options:
-            button.addItem(option)
-
-    def update_experiment_name_path(self):
-        self.experiment_name_path = os.path.join(os.getcwd(), 'thesis_data', self.experiment_name)
+    def publish_experiment_path(self):
+        pwd = os.path.join(self.get_experiment_name_path(), self.experiment_id)
+        self.pub_experiment_pwd.publish(pwd)
