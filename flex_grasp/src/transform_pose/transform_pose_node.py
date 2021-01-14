16ca64f27bb74cd2d1e467d7276a2449f9ea316e
TaekedeHaan <taeke.dehaan@live.nl>
2021-01-14 11:10:49 +0100

    Log transform pose, created seperate experiment info class

--- flex_grasp/src/transform_pose/transform_pose_node.py
+++ flex_grasp/src/transform_pose/transform_pose_node.py
@@ -8,10 +8,9 @@ from transform_pose import TransformPose
 
 
 NODE_NAME = 'transform_pose'
-NAME = "TRANSFORM POSE"
 DEFAULT_UPDATE_RATE = 10.0
 DEFAULT_DEBUG_MODE = True
-
+DEFAULT_PLAYBACK = False
 
 def main():
     debug_mode = rospy.get_param(NODE_NAME + "/debug", DEFAULT_DEBUG_MODE)
@@ -23,9 +22,10 @@ def main():
 
     rospy.init_node(NODE_NAME, anonymous=True, log_level=log_level)
     update_rate = rospy.get_param('~update_rate', DEFAULT_UPDATE_RATE)
+    playback = rospy.get_param("playback", DEFAULT_PLAYBACK)
 
     state_machine_input = StateMachineInput(NODE_NAME)
-    transform_pose = TransformPose(NODE_NAME)
+    transform_pose = TransformPose(NODE_NAME, playback=playback)
     transform_pose_state_machine = TransformPoseStateMachine(transform_pose, state_machine_input, update_rate, NODE_NAME)
     rospy.loginfo('[%s] Transform pose state machine successfully generated', NODE_NAME)
 
